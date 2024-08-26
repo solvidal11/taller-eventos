77120ec5b361a6592f5c3587f5bc42ec2ef7913a
solvidal11 <solvidal112002@gmail.com>
2024-08-26 14:33:38 -0300

    Correccion para div y saludo

--- eventos.js
+++ eventos.js
@@ -2,3 +2,7 @@ let botonDiv = document.getElementById('saludoDiv');
 botonDiv.addEventListener('click', function (){
     alert("Hola! Soy el div");
 });
+function saludar(event) {
+    event.stopPropagation(); // Detiene la propagación del evento
+    alert('¡Hola!');
+}
